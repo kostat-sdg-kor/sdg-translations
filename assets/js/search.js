dac9e8a420cb9b79e6bf9fbbb22faeb644204e86
kostat-sdg-kor <46581861+kostat-sdg-kor@users.noreply.github.com>
2021-07-01 07:55:17 +0000

    Deploying to gh-pages from  @ 3ef0498ebf60705ad70128f99b4d3427f356d085 🚀

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,9 +1,9 @@
 async function init() {
   try {
     const data = await Promise.all([
-      fetch('/sdg-translations/translations.json')
+      fetch('/sdg-translations-beta/translations.json')
         .then((response) => response.json()),
-      fetch('/sdg-translations/contexts.json')
+      fetch('/sdg-translations-beta/contexts.json')
         .then((response) => response.json())
     ]);
     const languages = data[0];
